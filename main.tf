83f40ba24509b0d4ac724ebdc57cd3164f757fba
huyeduon <huyeduon@cisco.com>
2021-02-02 09:44:37 +0800

    main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,19 @@
 terraform {
-  backend "remote"
+  backend "remote" {
+      hostname ="app.terraform.io"
+      organization="huyeduon"
+      workspaces {
+        name = "cloudcode"
+      }
+  }
+}
+
+provider "aws" {
+    region = "us-east-1"
+    access_key = var.aws_acc_key
+    secret_key = var.aws_sec_key
+}
+
+resource "aws_s3_bucket" "test" {
+    bucket_prefix = "test-"
 }
